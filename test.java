95770163d3f0f8da5c8ad3ea5a9efdf2f7bf24a4
Jesus <jfr1013@gmail.com>
2015-12-19 12:01:47 +0100

    Comprobadas todas las posibilidades en la clase Test

--- test.java
+++ test.java
@@ -1,8 +1,12 @@
 /**
  * Clase test para probar los diferentes métodos de la clase MusicOrganizer
  */
-public class test
+public class Test
 {
+    /**
+     * Método para probar la funcionalidad de la clase MusicOrganizer
+     * añadiendo varios archivos e invocando distintos métodos sobre el ArrayList que los contiene.
+     */
     public void testUsingAddFileMethod(){
         // Creamos el organizador de música
         MusicOrganizer archivos = new MusicOrganizer();
@@ -11,7 +15,7 @@ public class test
         archivos.addFile("archivo2");
         archivos.addFile("archivo3");
         // Obtenemos el número de archivos almacenados en el organizador
-        archivos.getNumberOfFiles();
+        System.out.println("Hay " + archivos.getNumberOfFiles() + " archivos");
         // Comprobamos el archivo almacenado en el indice pasado por parametro
         archivos.listFile(0);
         // Eliminamos uno de los archivos
@@ -21,10 +25,41 @@ public class test
         // Probamos un índice que esté fuera del rango
         archivos.checkIndex(5);
         // Comprobamos si es válido el indice introducido
-        archivos.validIndex(5);
+        System.out.println("El índice introducido es válido? " + archivos.validIndex(5));
         // Probamos un índice que esté dentro del rango
         archivos.checkIndex(1);
         // Comprobamos si es válido el indice introducido
-        archivos.validIndex(1);
+        System.out.println("El índice introducido es válido? " + archivos.validIndex(1));
+    }
+    
+    /**
+     * Método para comprobar si la clase MusicOrganizer sigue funcionando aunque
+     * no añadamos ningun archivo al ArrayList o se hayan borrado todos los que había.
+     */
+    public void testWithoutFiles(){
+        // Creamos el organizador de música
+        MusicOrganizer archivos = new MusicOrganizer();
+        // Obtenemos el número de archivos almacenados en el organizador
+        System.out.println("Hay " + archivos.getNumberOfFiles() + " archivos");
+        // Comprobamos si es válido un indice cualquiera
+        System.out.println("El índice introducido es válido? " + archivos.validIndex(0));
+        System.out.println("Ahora probaremos a añadir archivos y borrarlos todos para comprobar si funciona también");
+        // Añadimos los archivos
+        archivos.addFile("archivo1");
+        archivos.addFile("archivo2");
+        // Obtenemos el número de archivos almacenados en el organizador
+        System.out.println("Hay " + archivos.getNumberOfFiles() + " archivos");
+        // Comprobamos el archivo almacenado en el indice pasado por parametro
+        archivos.listFile(0);
+        // Eliminamos uno de los archivos
+        archivos.removeFile(0);
+        // Comprobamos el archivo almacenado en el indice pasado por parametro
+        archivos.listFile(0);
+        // Eliminamos el otro archivo
+        archivos.removeFile(0);
+        // Obtenemos el número de archivos almacenados en el organizador
+        System.out.println("Hay " + archivos.getNumberOfFiles() + " archivos");
+        // Comprobamos si es válido un indice cualquiera
+        System.out.println("El índice introducido es válido? " + archivos.validIndex(0));
     }
 }
